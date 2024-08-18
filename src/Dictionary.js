27f4f5ba29785b50fc3be03d9513e0c6c176cecb
Maxime <82838299+hallomaxime@users.noreply.github.com>
2024-08-18 14:32:11 +0200

    imported axios edited css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiKey = `dbaa90bd0tdaf4424ef37230ff2fcfo8`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
@@ -15,9 +25,13 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search} className="text-center">
-        <input type="search" onChange={handleKeywordChange} />
-        <input type="submit" className="btn btn-primary"></input>
+      <form onSubmit={search} className="row justify-content-center">
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          className="col-9 search"
+        />
+        <input type="submit" className="btn btn-primary col-2"></input>
       </form>
     </div>
   );
